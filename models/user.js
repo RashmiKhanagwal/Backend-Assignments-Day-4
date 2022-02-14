da57f184cca0acfee51838884f878ca0d336a20f
RashmiKhanagwal <rashmi.khanagwal@gmail.com>
2022-02-14 20:17:15 +0530

    user schema

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
     email: { type: String, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'] },
     phone:{ type: Number, required: [true, "User phone number required"], match: [/\d{3}-\d{3}-\d{4}/, 'is not a valid phone number!'] },
     created_at: { type: Date, required: true },
