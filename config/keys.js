3ec8410ce428ca2082a74117c7127445ad113d0b
RashmiKhanagwal <rashmi.khanagwal@gmail.com>
2022-02-22 15:00:26 +0530

    secret key

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secretOrKey: "s0m3$3Cret$h0lyC0d3&$"
+}
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,47 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken');
 
+const validateRegisterInput = require("../validation/register");
+const validateLoginInput = require("../validation/login");
+const User = require("../models/user");
+
+router.post('/signup',(req,res)=>{
+    // Form validation
+    const {errors, isValid} = validateRegisterInput(req.body);
+
+    //check validation
+    if(!isValid){
+        return res.status(400).json(errors);
+    }
+
+    User.findOne({email: req.body.email})
+    .then(user=>{
+        if(user){
+            return res.status(400).json({ email: "Email already exists" });
+        } else {
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                phone: req.body.phone,
+                password: req.body.password
+            });
+
+            // Hash password before saving in database
+            bcrypt.genSalt(10, (err, salt)=>{
+                bcrypt.hash(newUser.password, salt, (err,hash)=>{
+                    if(err) throw err;
+                    newUser.password = hash;
+                    newUser.save()
+                    .then(user => res.json(user))
+                    .catch(err => console.log(err));
+                });
+            });
+        };
+    });
+});
 
 router.post('/signup', passport.authenticate('signup', {session:false}),
     async(req,res,next)=>{
