2b5878ebf597134fb2aeff6c8c9754330783bd48
RashmiKhanagwal <rashmi.khanagwal@gmail.com>
2022-02-22 15:26:37 +0530

    server.js file

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const passport = require("passport");
+const users = require("./routes/user");
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -13,5 +15,13 @@ mongoose
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
+// passport middleware
+app.use(passport.initialize());
+
+// passport config
+require("./config/passport")(passport);
+
+// routes
+app.use("/api/users", users);
 
 app.listen(3000, () => {console.log("server connected")});
