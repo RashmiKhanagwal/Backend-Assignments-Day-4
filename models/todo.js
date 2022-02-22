af30a2d3bef249d2aea1c183e564819257807743
RashmiKhanagwal <rashmi.khanagwal@gmail.com>
2022-02-22 15:48:34 +0530

    todo schema

--- models/todo.js
+++ models/todo.js
@@ -4,10 +4,14 @@ const TodoSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
     title: { type: String, required: true },
-    is_Complete:{ type: Boolean, required: true },
+    is_Complete:{ type: Boolean, required: true, default:false },
     created_at: { type: Date, required: true },
     updated_at: { type: Date, required: true },
-    category: { type: String, required: true }
+    category: { type: Array },
+    userId:{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }
   },
   { timestamps: true }
 );
